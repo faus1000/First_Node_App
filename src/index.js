80247853cc6325bdbfa9046351ffffa632d8e8d3
fausto <operyde@gmail.com>
2021-01-31 19:57:54 -0400

    Puerto dinamico agregado

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ const app = express();
 const path = require('path');
 
 //settings
-app.set('port',4000);
+app.set('port', process.env.PORT || 4000);
 app.set('views',path.join(__dirname,'views'));
 app.engine('html',require('ejs').renderFile);
  app.set('view engine','ejs');
